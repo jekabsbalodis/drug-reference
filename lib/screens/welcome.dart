d5ec4c65c3a4c2a7261d1e9db4ad3ed3954503dc
jekabs.balodis <jekabs.balodis@gmail.com>
2024-05-21 10:41:13 +0000

    Moving checkbox to middle of screen

--- lib/screens/welcome.dart
+++ lib/screens/welcome.dart
@@ -114,7 +114,7 @@ class _WelcomeScreenState extends State<WelcomeScreen> {
               ),
               const SizedBox(height: 24),
               Row(
-                mainAxisAlignment: MainAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.center,
                 children: [
                   SizedBox(
                     height: 32,
