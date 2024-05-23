84f5713cd441395f69faeafecf3176d4f5bda398
jekabsbalodis <signalingpigeon@duck.com>
2024-05-24 00:20:43 +0300

    Creating draft of medication search modal

--- lib/widgets/medication_search.dart
+++ lib/widgets/medication_search.dart
@@ -1,15 +1,28 @@
 import 'package:flutter/material.dart';
 
 class MedicationSearch extends StatefulWidget {
-  const MedicationSearch({
-    super.key,
-  });
+  const MedicationSearch({super.key, required this.onEnterSearchTerm});
+
+  final void Function(String searchTerm) onEnterSearchTerm;
 
   @override
   State<MedicationSearch> createState() => _MedicationSearchState();
 }
 
 class _MedicationSearchState extends State<MedicationSearch> {
+  final _searchTermController = TextEditingController();
+
+  void _submitSearchTerm() {
+    widget.onEnterSearchTerm(_searchTermController.text);
+    Navigator.pop(context);
+  }
+
+  @override
+  void dispose() {
+    _searchTermController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final keyboardSpace = MediaQuery.of(context).viewInsets.bottom;
@@ -17,10 +30,29 @@ class _MedicationSearchState extends State<MedicationSearch> {
     return SingleChildScrollView(
       child: Padding(
         padding: EdgeInsets.fromLTRB(16, 16, 16, keyboardSpace + 16),
-        child: const Column(children: [Row(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [Expanded(child: TextField())],
-        )],),
+        child: Column(
+          children: [
+            Row(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Expanded(
+                  child: TextField(
+                      controller: _searchTermController,
+                      keyboardType: TextInputType.text,
+                      decoration: const InputDecoration(
+                          label: Text('Meklēt medikamentu')),
+                      style: Theme.of(context).textTheme.bodyMedium!.copyWith(
+                          color: Theme.of(context).colorScheme.onSurface),
+                      onSubmitted: (value) {
+                        _submitSearchTerm();
+                      }),
+                ),
+                ElevatedButton(
+                    onPressed: _submitSearchTerm, child: const Text('Meklēt'))
+              ],
+            )
+          ],
+        ),
       ),
     );
   }
